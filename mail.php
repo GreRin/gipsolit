f450241374b6c53e2e5be8fd89fc3da24f08cec4
GreRin <rigor1988@gmail.com>
2019-12-24 13:25:39 +0300

    feat: upload files

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+echo "Информация принята <br>";
+$name = $_POST['user_name'];
+echo "Ваше имя - $name " . $name;
+$email = $_POST['email'];
+echo " <br> Ваше email - $email <br>";
+$user_phone = $_POST['user_phone'];
+echo "Ваше телефон - $user_phone <br>";
+$site = $_POST['site'];
+echo "Ваше телефон - $site <br>";
+?>
